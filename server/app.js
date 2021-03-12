986254f4edc02d4eb54e6fce4ff0cc3a55ec6a8b
Turner Jimerson <turnerj344@gmail.com>
2021-03-12 08:50:07 -0600

    setting up env

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
-const Express = require('express');
+const Express = require("express");
 const app = Express();
- 
-app.listen(3000, () => {
-console.log(`[Server]: App is listening on 3000.`);
+``
+app.use("/test", (req, res) => {
+  res.send("This is a message from the test endpoint on the server!");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`[Server]: App is listening on ${process.env.PORT}.`);
 });

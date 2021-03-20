43843609eec1284b52e4e1eb10efc9e9f7abbd99
NHUTTON <nickolashutton@gmail.com>
2021-03-20 16:10:02 -0400

    testing server

--- Controllers/gamesController.js
+++ Controllers/gamesController.js
@@ -1,6 +1,6 @@
 const Express = require('express');
 const router = Express.Router();
-let validateJWT = require("../middleware/validate-jwt");
+let {validateJWT} = require("../middleware");
 const {GamesModel} = require('../models')
 
 router.get('/test', validateJWT, (req,res) => {
--- Controllers/userController.js
+++ Controllers/userController.js
@@ -4,10 +4,9 @@ const {UniqueConstraintError} = require('sequelize/lib/errors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-//New User
+
 router.post('/register', async (req, res) => {
-    /*this is the server side portion that creates the token that we are accessing in the client side and storing it in our browser to allow access to the protected parts of our database --this is for the register component, the login portion is further down witha very similar structure
-    */
+
     const {username, password} = req.body.user;
     
     try {
@@ -37,8 +36,7 @@ router.post('/register', async (req, res) => {
     }
 });
 
-//User login
-router.post('/login', async (req, res) => {   //<------heres where we are using our user model to input username and password into our user model {request body} sent by the user.
+router.post('/login', async (req, res) => {  
     const {username, password} = req.body.user;
 
     try {
@@ -52,13 +50,12 @@ router.post('/login', async (req, res) => {   //<------heres where we are using 
 
             let passwordComp = await bcrypt.compare(password, loginUser.password)
 
-            if (passwordComp) {  //<----- this is checking to make sure our password and login combination works and if it does it is suppling our session token from our dependencies
+            if (passwordComp) {  
                 let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET,  {expiresIn: 60*60*24});  
-
                 res.status(200).json({    
                     message: "Login Successful!",
                     username: loginUser,
-                    sessionToken: token      //<---- supplying our session token
+                    sessionToken: token     
                 });
             } else {
                 res.status(401).json({
